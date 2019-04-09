2d7028bac2b30bfd307bdee44d0a307a21084bd0
kyohei tsukuda <tsukuda.kyouhei@gmail.com>
2019-04-09 22:44:23 +0900

    update googleapis v39

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 
 const querystring = require('querystring')
 const url = require('url')
-const google = require('googleapis')
+const { google } = require('googleapis')
 const uuid = require('uuid')
 const redirect = require('micro-redirect')
 
 const provider = 'google';
-const {OAuth2} = google.auth
 const plus = google.plus('v1')
 const DEFAULT_SCOPES = [
   'https://www.googleapis.com/auth/userinfo.email'
@@ -24,10 +23,10 @@ const getToken = (oauth2Client, code) => new Promise((resolve, reject) => {
 })
 
 const getUser = oauth2Client => new Promise((resolve, reject) => {
-  plus.people.get({userId: 'me', auth: oauth2Client}, (err, response) => {
+  plus.people.get({ userId: 'me', auth: oauth2Client }, (err, response) => {
     if (err) return reject(err)
 
-    resolve(response)
+    resolve(response.data)
   })
 })
 
@@ -40,12 +39,11 @@ const microAuthGoogle = ({
   accessType = 'offline'
 }) => {
   const states = [];
-  const oauth2Client = new OAuth2(clientId, clientSecret, callbackUrl)
-
+  const oauth2Client = new google.auth.OAuth2(clientId, clientSecret, callbackUrl);
   scopes = DEFAULT_SCOPES.concat(scopes)
 
   return fn => async (req, res, ...args) => {
-    const {pathname, query} = url.parse(req.url)
+    const { pathname, query } = url.parse(req.url)
 
     if (pathname === path) {
       try {
@@ -61,7 +59,7 @@ const microAuthGoogle = ({
 
         return redirect(res, 302, redirectUrl)
       } catch (err) {
-        args.push({err, provider})
+        args.push({ err, provider })
         return fn(req, res, ...args)
       }
     }
@@ -69,11 +67,11 @@ const microAuthGoogle = ({
     const callbackPath = url.parse(callbackUrl).pathname
     if (pathname === callbackPath) {
       try {
-        const {state, code} = querystring.parse(query)
+        const { state, code } = querystring.parse(query)
 
         if (!states.includes(state)) {
           const err = new Error('Invalid state')
-          args.push({err, provider})
+          args.push({ err, provider })
           return fn(req, res, ...args)
         }
 
@@ -82,7 +80,7 @@ const microAuthGoogle = ({
         const tokens = await getToken(oauth2Client, code)
 
         if (tokens.error) {
-          args.push({err: tokens.error, provider});
+          args.push({ err: tokens.error, provider });
           return fn(req, res, ...args);
         }
 
@@ -94,11 +92,11 @@ const microAuthGoogle = ({
           tokens
         }
 
-        args.push({result})
+        args.push({ result })
 
         return fn(req, res, ...args)
       } catch (err) {
-        args.push({err, provider})
+        args.push({ err, provider })
         return fn(req, res, ...args)
       }
     }
