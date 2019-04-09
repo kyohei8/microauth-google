6aaf8213b52a39d60ce955866ce7e1bb93c1e713
kyohei tsukuda <tsukuda.kyouhei@gmail.com>
2019-04-09 22:55:39 +0900

    To change hold it as a cookie because state is not available at `now`

--- index.js
+++ index.js
@@ -5,12 +5,43 @@ const url = require('url')
 const { google } = require('googleapis')
 const uuid = require('uuid')
 const redirect = require('micro-redirect')
+const cookie = require('cookie');
 
 const provider = 'google';
 const plus = google.plus('v1')
 const DEFAULT_SCOPES = [
   'https://www.googleapis.com/auth/userinfo.email'
 ]
+const google_auth_state = 'c8xPwF04wf'
+function parseCookies(request) {
+  const list = {}, rc = request.headers.cookie;
+
+  rc && rc.split(';').forEach(function (cookie) {
+    var parts = cookie.split('=');
+    list[parts.shift().trim()] = decodeURI(parts.join('='));
+  });
+
+  return list;
+}
+
+function genCookies(key, val) {
+  // 1hour
+  const expiresIn = 60 * 60 * 1000;
+  const options = {
+    maxAge: expiresIn,
+    httpOnly: true
+  };
+  return cookie.serialize(key, val, options);
+}
+
+function deleteCookies(key) {
+  const expiresIn = 0;
+  const options = {
+    maxAge: expiresIn,
+    httpOnly: true
+  };
+  return cookie.serialize(key, '**', options);
+}
 
 const getToken = (oauth2Client, code) => new Promise((resolve, reject) => {
   oauth2Client.getToken(code, (err, tokens) => {
@@ -49,6 +80,9 @@ const microAuthGoogle = ({
       try {
         const state = uuid.v4()
         states.push(state)
+        // set state to cookie
+        const cookie = genCookies(google_auth_state, states.join(','));
+        res.setHeader('Set-Cookie', cookie);
 
         const redirectUrl = oauth2Client.generateAuthUrl({
           // eslint-disable-next-line camelcase
@@ -68,11 +102,18 @@ const microAuthGoogle = ({
     if (pathname === callbackPath) {
       try {
         const { state, code } = querystring.parse(query)
-
+        const cookies = parseCookies(req);
+        // delete cookie
+        const delCookie = deleteCookies(google_auth_state);
+        res.setHeader('Set-Cookie', delCookie);
         if (!states.includes(state)) {
-          const err = new Error('Invalid state')
-          args.push({ err, provider })
-          return fn(req, res, ...args)
+          // get state by cookies
+          const states = cookies[google_auth_state];
+          if (states && !states.split('%2C').includes(state)) {
+            const err = new Error('Invalid state')
+            args.push({ err, provider })
+            return fn(req, res, ...args)
+          }
         }
 
         states.splice(states.indexOf(state), 1)
